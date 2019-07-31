e0a47088fc856e649385e10e745caccf75a866cf
mochenghui <mochenghui@gomo.com>
2019-07-31 16:57:04 +0800

    增加grpc代码

---
+++ grpc/src/main/java/grpc/examples/HelloWorldClient.java
@@ -0,0 +1,58 @@
+package grpc.examples;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+/**
+ * @author mochenghui
+ * @date 2019/7/31 14:50
+ */
+public class HelloWorldClient {
+
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public HelloWorldClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext(true)
+                .build());
+    }
+
+    public HelloWorldClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    private void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    private void greet(String user) {
+        Helloworld.HelloRequest request = Helloworld.HelloRequest.newBuilder().setName(user).build();
+        Helloworld.HelloReply response = blockingStub.sayHello(request);
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+            /* Access a service running on the local machine on port 50051 */
+            String user = "world";
+            if (args.length > 0) {
+                user = args[0]; /* Use the arg as the name to greet if provided */
+            }
+            client.greet(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+
+
+}
